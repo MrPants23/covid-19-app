8f9e6ad5559eb255a30988725275860a67528853
Diego.Monroy <diego.monroy@mouser.com>
2022-09-16 07:31:01 -0500

    Covid-19 Tracker App (Mat-UI)

---
+++ src/components/CountryPicker/CountryPicker.jsx
@@ -0,0 +1,37 @@
+import React, { useState, useEffect } from 'react';
+import { NativeSelect, FormControl } from '@mui/material';
+import styles from './CountryPicker.module.css';
+import { fetchCountries } from '../../api';
+
+const CountryPicker = ({ handleCountryChange }) => {
+
+    const [fetchedCountries, setFetchedCountries] = useState([]);
+
+    useEffect(() => {
+        const fetchAPI = async () => {
+            setFetchedCountries(await fetchCountries())
+        }
+
+        fetchAPI();
+    }, [setFetchedCountries]);
+
+    return (
+        <FormControl className={styles.formControl}>
+            <NativeSelect defaultValue="" onChange={(e) => handleCountryChange(e.target.value)}>
+                <option value="">Global</option>
+                {
+                    fetchedCountries
+                        ?
+                        (
+                            fetchedCountries.map(
+                                (country, i) => <option key={i} value={country.name}>{country.name}</option>
+                            )
+                        )
+                        : null
+                }
+            </NativeSelect>
+        </FormControl>
+    )
+}
+
+export default CountryPicker;
